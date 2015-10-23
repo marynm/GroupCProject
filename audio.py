c4d2cf2e48ef8d89141d9b8d0935d2ba5d9d158f
marynm <marynm@mac.com>
2015-10-23 16:42:44 +0000

    added print statements to audio.py

--- audio.py
+++ audio.py
@@ -7,9 +7,11 @@ directoryList = os.listdir(directory)
 def play_song(song):
 	
 	if(song in directoryList):
+		print('Playing: ' + str(song))
 		os.system('mplayer ' + song)
 		return 'Playing:' + str(song)
 	else:
+		print('Song "' + song + '" does not exist.')
 		return 'Song "' + song + '" does not exist.'
 #while 1:
 	#play_song(input('song pls'))
