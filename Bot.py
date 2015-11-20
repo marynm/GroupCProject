759639933c936993cb146858a10c6f77eb2d7bd0
Kevinrmiller93 <kevinrmiller@cmail.carleton.ca>
2015-11-20 18:42:10 +0000

    The bot code

--- Bot.py
+++ Bot.py
@@ -5,35 +5,37 @@ import sys
    
 
 def receive_command():
-"""wait for commands from the controller and process them"""
+#wait for commands from the controller and process them
 	while(1):
-		#data = s.recv(BUFFER_SIZE)
-		#command = data.decode("utf-8")
-		command = "3"
-		print("received data:", command)
+		print("test1")
+		data = s.recv(BUFFER_SIZE)
+		print("test2")
+		if(data):
+			command = data.decode("utf-8")
+			print("received data:", command)
 
-		if (command == "camera"):
-			take_picture()
+			if (command == "camera"):
+				take_picture()
 	
-		elif (command == "music"):
-			next_song()
+			elif (command == "music"):
+				next_song()
 		
-		else:
-			move_bot(command)
+			else:
+				move_bot(command)
 		
 
 
 def move_bot(position):
-"""move to location and tell controller when reached"""
+#move to location and tell controller when reached"""
 	print("Moving bot to position" + position);
 	
 	#once at position send message to controller indicating it has arrived
-	MESSAGE = bytes("at location", "utf-8")
-	s.send(MESSAGE)
+	#MESSAGE = bytes("at location", "utf-8")
+	#s.send(MESSAGE)
 
 
 def take_picture():
-"""take pictures"""
+#take pictures"""
 #fswebcam image.jpg
 	print("Taking picture")
 	
@@ -55,12 +57,12 @@ def next_song():
 	print("Playing next song")
 	#move to next item in the directory list
 	#song = directoryList[i] 	#?????
-	play_song(song)
+	#play_song(song)
 
 
 
 def get_files(dirname):
-    """ Return list of file paths in directory """
+    #Return list of file paths in directory
 
     # Get list of files
     filepaths = []
@@ -75,15 +77,15 @@ def get_files(dirname):
 if __name__ == '__main__':
 	#setup TCP connection
 	TCP_IP = '10.0.0.32'
-	TCP_PORT = 5005
+	TCP_PORT = 5002
 	BUFFER_SIZE = 1024
 
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	#s.connect((TCP_IP, TCP_PORT))
+	s.connect((TCP_IP, TCP_PORT))
 	
 	#prepare the list of songs
-	directory = "./Music"		#????
-	directoryList = get_files(directory) #os.listdir(directory)
+	#directory = "./Music"		#????
+	#directoryList = get_files(directory) #os.listdir(directory)
 	
 	while 1:
 		receive_command()
