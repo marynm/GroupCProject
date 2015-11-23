9c63dd3d33feafa855acd8f0e0142433b5d1c2a1
Preeti <preetinigam@cmail.carleton.ca>
2015-11-23 14:16:22 -0500

    Update DispenserTest.py

--- DispenserTest.py
+++ DispenserTest.py
@@ -4,22 +4,13 @@ import time
 import threading
 
 class HardwareInterface(threading.Thread):
-def_init_(self):
-	super(HardwareInterface, self)._init_()
-	self.setDaemon(True)
-	self.Running = False
-	self.Dispensing = False
-	self.down_cb = []
-	self.up_cb = []
-	self.feed_cb = []
+
 	
-	
+
 def Dispense(self):
-	if not self.dispensing:
-		self.step=0
-		self.Dispensing = True
-		for cb in self.feed_cb:
-		cb()
+	#check if dispenser turns movs from initial position
+		dispenseMotor.dispense()
+		
 
 
 #test clsoing of the dispenser door then reclosing it
