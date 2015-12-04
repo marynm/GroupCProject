18cc7f80ba2f6080cc7376e586c13b1d1c045be5
Kevinrmiller93 <kevinrmiller@cmail.carleton.ca>
2015-12-04 15:47:59 +0000

    updated dispenser angle

--- dispenserToTest.py
+++ dispenserToTest.py
@@ -6,16 +6,16 @@ import time
 
 def dispense():
 #open the dispenser door for 3 seconds, then reclose it
-		p.ChangeDutyCycle(7.5)
+		p.ChangeDutyCycle(7)
 		time.sleep(3)
 		p.ChangeDutyCycle(12.5)
 
 if __name__ == '__main__':
 	#prepare to receive UPD messages from controller
-	UDP_IP = "10.0.0.21"
-	UDP_PORT = 5005
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-	sock.bind((UDP_IP, UDP_PORT))
+#	UDP_IP = "10.0.0.21"
+#	UDP_PORT = 5005
+#	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+#	sock.bind((UDP_IP, UDP_PORT))
 
 
 	#prepare stepper motor, starting it in closed position
@@ -28,8 +28,9 @@ if __name__ == '__main__':
 	try:
 		#wait for messages from cotroller and operate dispenser when prompted
 		while True:
-			data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-			print( "received message:" + data.decode("utf-8"))
+#			data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+#			print( "received message:" + data.decode("utf-8"))
+			time.sleep(5)	
 			dispense()
 
 	except KeyboardInterrupt:
