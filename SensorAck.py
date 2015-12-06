a8c11293a1e8cdf9f4de000293ac196057775ae7
marynm <marynm@mac.com>
2015-12-06 11:38:53 -0500

    cleanup of files

--- SensorAck.py
+++
@@ -1,9 +0,0 @@
-class SensorAck(object):
-    def __init__(self, SensorAck):
-        self.SensorAck = SensorAck
-
-class ChipsDispenser(object):
-    def getSensorAck(self):
-        newTurnNumber = TurnNumberSequence.next_turn_number()
-        newSensorAck = SensorAck(newTurnNumber)
-        return newSensorAck
